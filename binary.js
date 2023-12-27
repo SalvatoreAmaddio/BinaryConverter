b411bee1342d5236eb6d745a1e538a7272de9b00
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2023-12-27 15:51:14 +0000

    j

--- binary.js
+++ binary.js
@@ -10,50 +10,107 @@ class Bit
 
 class Nibble
 {
-    bits = []
-    len = 4;
-    full = false;
-    constructor() 
+    #bits = [new Bit(), new Bit(), new Bit(), new Bit()]
+
+    get len() 
     {
-        for(let i = 0; i < this.len; i++)
-            this.bits[i] = new Bit();
+        return this.#bits.length;
     }
 
-    add(index, value) 
+    reverseBits() 
     {
+        this.#bits.reverse();
+    }
+
+    addBit(index, value) 
+    {
+        if (index > this.len-1) 
+            throw "Nibbles can have only 4 bits";
+
         let bit = new Bit();
         bit.value = value;
-        this.bits[index] = bit;
-        if (index == 3) this.full = true;
+        this.#bits[index] = bit;
+    }
+
+    get firstBit() 
+    {
+        return this.#bits[0];
+    }
+
+    get lastBit() 
+    {
+        return this.#bits[this.len-1];
+    }
+
+    getBit(index) 
+    {
+        return this.#bits[index];
     }
 
     toString() 
     {
-        return this.bits.toString();
+        return this.#bits.toString();
     }
 }
 
 class Byte extends Nibble 
 {
-    len = 8;
-    constructor() 
+    #index = 0;
+    #bits = [new Bit(), new Bit(), new Bit(), new Bit(), new Bit(), new Bit(), new Bit(), new Bit()];
+
+    addNibble(nibble) 
     {
-        super();
+        for(let i = 0; i < nibble.len; i ++) 
+        {
+            this.#bits[this.#index] = nibble.getBit(i);
+            this.#index++;
+        }
+    }
+
+    get full() 
+    {
+        return this.#index == this.len-1;
+    }
+
+    getFirstNibble() 
+    {
+        let nibble = new Nibble();
+        for(let i = 0; i < 4; i++) 
+            nibble.addBit(this.#bits[i]);
+        return nibble;
+    }
+
+    getLastNibble() 
+    {
+        let nibble = new Nibble();
+        for(let i = 4; i < this.len; i++) 
+            nibble.addBit(this.#bits[i]);        
+        return nibble;
     }
 }
 
 class Convert
 {
-    number=0;
+    #number=0;
     #result = 0;
     #base = 2;
     #power = 0;
-    binPlaceValues = [];
+    #binPlaceValues = [];
+    #nibbles = [];
+    #bytes = [];
+    #str;
 
     constructor(number) 
     {
-        this.number = number;
+        this.#number = number;
         this.calculatePowers();
+        this.#buildUpToString();
+        this.#breakIntoNibbles();
+    }
+
+    get number()
+    {
+        return this.#number;
     }
 
     calculatePowers() 
@@ -64,63 +121,88 @@ class Convert
         while(true) 
         {
             this.#result = this.#base ** this.#power;
-            write(`${this.#base} ^ ${this.#power} = ${this.#result}`);
             powers[i] = this.#result;
             bin[i] = 0;
 
             this.#power++;
             i++;
-            if (this.#result > this.number) break;
+            if (this.#result > this.#number) break;
         }
         
         powers.reverse();
-        this.binPlaceValues.push([powers, bin]);
-        this.printBinPlaceValues();
+        this.#binPlaceValues.push([powers, bin]);
     }
 
-    printBinPlaceValues() 
+    #buildUpToString() 
     {
-        document.writeln(`<table>`);   
+        this.#str = `<table><tr>`;
         let powers;
         let result;
-        document.writeln(`<tr>`);   
-        for(let r = 0 ; r < this.binPlaceValues[0][0].length; r++) 
+        for(let r = 0 ; r < this.#binPlaceValues[0][0].length; r++) 
         {
-            powers = this.binPlaceValues[0][0][r];
-            result = this.number - powers;
-//            console.log(`${this.number} - ${powers} = ${result}`);
-            document.writeln(`<th>${powers}</th>`); 
+            powers = this.#binPlaceValues[0][0][r];
+            result = this.#number - powers;
+            this.#str = this.#str + `<th>${powers}</th>`;
             
             if (result >= 0) 
             {
-                this.binPlaceValues[0][1][r] = 1;
-                this.number = result;
+                this.#binPlaceValues[0][1][r] = 1;
+                this.#number = result;
             }
         }
 
-        document.writeln(`</tr>`);        
-        document.writeln(`<tr>`);   
+        this.#str = this.#str + `</tr><tr>`;
 
-        for(let r = 0 ; r < this.binPlaceValues[0][0].length; r++) 
+        for(let r = 0 ; r < this.#binPlaceValues[0][0].length; r++) 
         {
-            let binVal = this.binPlaceValues[0][1][r];
-            document.writeln(`<td>${binVal}</td>`); 
+            let binVal = this.#binPlaceValues[0][1][r];
+            this.#str = this.#str + `<td>${binVal}</td>`;
         } 
-        
-        document.writeln(`</tr>`);
-        document.writeln(`</table>`);   
+
+        this.#str = this.#str + `</tr></table>`;
     }
 
-    write(n) 
+    #breakIntoNibbles() 
     {
-        document.writeln(`<p>${n}</p>`);   
+        let nibble = new Nibble();
+        let bit = 0;
+        this.#binPlaceValues[0][1].reverse();
+        
+        for(let r = 0 ; r < this.#binPlaceValues[0][1].length; r++) 
+        {
+            let binVal = this.#binPlaceValues[0][1][r];
+
+            if (bit > 3 || r == this.#binPlaceValues[0][0].length-1) 
+            {
+                bit = 0;
+                nibble.reverseBits();
+                this.#nibbles.push(nibble);                
+                nibble = new Nibble();
+            }
+            nibble.addBit(bit,binVal);
+            bit++;
+        } 
+        this.#nibbles.reverse();
     }
 
-}
+    get nibblesCount() 
+    {
+        return this.#nibbles.length;
+    }
+    
+    printNibbles() 
+    {
+        for(let n = 0; n < this.#nibbles.length; n++)
+            write(this.#nibbles[n]);
+    }
 
-let conv = new Convert(64);
+    getNibble(index) 
+    {
+        return this.#nibbles[index];
+    }
 
-function write(n) 
-{
-    document.writeln(`<p>${n}</p>`);   
+    toString() 
+    {
+        return this.#str;
+    }
 }
